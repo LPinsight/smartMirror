97e3c894332dc0939814b1ee59b82900e6dcc708
LPinsight <LPinsight@users.noreply.github.com>
2025-11-08 16:28:23 +0100

    ✨ Verbesserung der Thread-Sicherheit im WebSocketService: Hinzufügen von Mutex-Sperren zur sicheren Verwaltung von Clients und Broadcast-Nachrichten.

--- api/websocket/websocket.go
+++ api/websocket/websocket.go
@@ -2,6 +2,7 @@ package websocket
 
 import (
 	"net/http"
+	"sync"
 
 	"github.com/gorilla/websocket"
 )
@@ -10,6 +11,7 @@ type WebSocketService struct {
 	clients   map[*websocket.Conn]bool
 	broadcast chan string
 	upgrader  websocket.Upgrader
+	mutex     sync.RWMutex
 }
 
 func NewWebSocketService() *WebSocketService {
@@ -26,13 +28,21 @@ func (ws *WebSocketService) Run() {
 	go func() {
 		for {
 			msg := <-ws.broadcast
+
+			ws.mutex.RLock()
 			for client := range ws.clients {
 				err := client.WriteMessage(websocket.TextMessage, []byte(msg))
 				if err != nil {
 					client.Close()
+
+					ws.mutex.RUnlock()
+					ws.mutex.Lock()
 					delete(ws.clients, client)
+					ws.mutex.Unlock()
+					ws.mutex.RLock()
 				}
 			}
+			ws.mutex.RUnlock()
 		}
 	}()
 }
@@ -45,12 +55,16 @@ func (ws *WebSocketService) Handler(w http.ResponseWriter, r *http.Request) {
 
 	defer conn.Close()
 
+	ws.mutex.Lock()
 	ws.clients[conn] = true
+	ws.mutex.Unlock()
 
 	for {
 		_, msg, err := conn.ReadMessage()
 		if err != nil {
+			ws.mutex.Lock()
 			delete(ws.clients, conn)
+			ws.mutex.Unlock()
 			break
 		}
 
